117bf46e867f166b8b767d07c0bee7fc7420d310
jaimeviloriogreen <jaimeviloriogreen@hotmail.com>
2025-01-27 14:00:06 -0400

    classes

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -12,7 +12,10 @@ import { StudentsService } from "./students.service";
 import { CreateStudentDto } from "./dto/create-student.dto";
 import { UpdateStudentDto } from "./dto/update-student.dto";
 import { UUID } from "crypto";
+import { Role } from "src/roles/decorators/role.decorator";
+import { RolesEnum } from "src/roles/enums/role.enum";
 
+@Role(RolesEnum.ADMIN)
 @Controller("students")
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
